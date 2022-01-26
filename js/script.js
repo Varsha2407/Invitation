60107b8d0b013d6c8a2cf98faf7ce85694f088c8
Varsha2407 <78837558+Varsha2407@users.noreply.github.com>
2022-01-26 09:00:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 18, 2021 23:27:00").getTime();
+var countDownDate = new Date("Feb 10, 2022 23:27:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
